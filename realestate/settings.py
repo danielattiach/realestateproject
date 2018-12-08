abcc311a49920bdb60f0b58ff72203e125d32ec2
Daniel Attiach <40741022+danielattiach@users.noreply.github.com>
2018-12-08 18:20:23 +0200

    Update settings.py

--- realestate/settings.py
+++ realestate/settings.py
@@ -145,7 +145,7 @@ MESSAGE_TAGS = {
 # Email config
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'daniel.portfolio.messages@gmail.com'
-EMAIL_HOST_PASSWORD = '321321Aa'
+EMAIL_HOST_USER = 'email'
+EMAIL_HOST_PASSWORD = 'password'
 EMAIL_USE_TLS = True
 
